19a0fc156cb1549645324565f57f5992cb902ced
jfaulkner9292 <jerrison9292@gmail.com>
2020-02-22 01:01:49 -0600

    js commit test

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /*import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+Test
+
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
